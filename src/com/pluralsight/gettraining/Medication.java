452aaf77728e264142331352f3f55dc56fc6f7e3
Mirna Viquez <mirna.viquez@gorillalogic.com>
2017-06-23 09:43:08 -0600

    Add, update, select, remove for Medication

--- src/com/pluralsight/gettraining/Medication.java
+++ src/com/pluralsight/gettraining/Medication.java
@@ -1,23 +1,50 @@
 package com.pluralsight.gettraining;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by vivianaaraya on 6/21/17.
  */
 public class Medication {
 
+    public int medicationId;
     public String type;
     public String name;
     public String treatment;
     public String dosage;
-    public String contraindications;
+    public String contraIndications;
+    public List<Medication> medicationList = new ArrayList<Medication>();
 
-    Medication(String type, String name, String treatment, String dosage, String contraindications){
+    Medication(){
 
-        this.type = type;
-        this.name = name;
-        this.treatment = treatment;
-        this.dosage = dosage;
-        this.contraindications = contraindications;
     }
+
+    public void addMedication(Medication medication){
+        medicationList.add(medication);
+    }
+
+    public Medication selectMedication(int medId){
+        int pos= medicationList.indexOf(medId);
+        return medicationList.get(pos);
+
+    }
+
+    public Medication updateMedication(Medication medication){
+        int pos= medicationList.indexOf(medicationId);
+        Medication toDelete= medicationList.get(pos);
+        medicationList.remove(toDelete);
+        medicationList.add(medication);
+        return medication;
+
+    }
+
+    public void deleteMedication(int medId){
+        int pos= medicationList.indexOf(medId);
+        Medication toDelete= medicationList.get(pos);
+        medicationList.remove(toDelete);
+
+    }
+
 
 }

a0145d32555c71ea38d81785994825e5d5ce08e2
Mirna Viquez <mirna.viquez@gorillalogic.com>
2017-06-23 09:36:21 -0600

    Add, select, update, remove for Prescription

--- src/com/pluralsight/gettraining/Prescription.java
+++ src/com/pluralsight/gettraining/Prescription.java
@@ -1,24 +1,50 @@
 package com.pluralsight.gettraining;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by vivianaaraya on 6/21/17.
  */
 public class Prescription {
 
+    public int prescriptionId;
     public String role;
     public String medication;
     public String dose;
     public String frequency;
     public Date filledDate;
+    public List<Prescription> prescriptionList = new ArrayList<Prescription>();
 
-    Prescription (String role, String medication, String dose, String frequency, Date filledDate){
+    Prescription (){
 
-        this.role = role;
-        this.medication = medication;
-        this.dose = dose;
-        this.frequency = frequency;
-        this.filledDate = filledDate;
     }
+
+    public void addPrescription(Prescription prescription){
+        prescriptionList.add(prescription);
+    }
+
+    public Prescription selectPrescription(int prescId){
+        int pos= prescriptionList.indexOf(prescId);
+        return prescriptionList.get(pos);
+
+    }
+
+    public Prescription updatePrescription(Prescription prescription){
+        int pos= prescriptionList.indexOf(prescriptionId);
+        Prescription toDelete= prescriptionList.get(pos);
+        prescriptionList.remove(toDelete);
+        prescriptionList.add(prescription);
+        return prescription;
+
+    }
+
+    public void deletePrescription(int prescId){
+        int pos= prescriptionList.indexOf(prescId);
+        Prescription toDelete= prescriptionList.get(pos);
+        prescriptionList.remove(toDelete);
+
+    }
+
 }
